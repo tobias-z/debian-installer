5faeda479e5364d745e12bc2cb254b0e6d095f34
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-07-01 23:50:13 +0200

    add -y to remove prompt

--- bin/pre-installers/git.sh
+++ bin/pre-installers/git.sh
@@ -6,7 +6,7 @@ if [ "$2" = "1" ]; then
     exit 0
 fi
 
-sudo apt install make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
+sudo apt install make build-essential libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip -y
 
 cd /tmp
 sudo wget https://github.com/git/git/archive/master.zip -O git.zip
@@ -24,8 +24,7 @@ fi
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+sudo apt install gh -y
 
 echo "Enter Github access token:"
 read access_token
