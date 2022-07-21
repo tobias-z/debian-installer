68f924fa37ff1f1604a24325e9c8a1e76e635ede
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-07-21 22:04:44 +0200

    dont use loop

--- bin/install.sh
+++ bin/install.sh
@@ -24,18 +24,34 @@ while true; do
     esac
 done
 
-execute_file () {
-    local file_name=$1
-    for file in $file_name; do
-        echo "Installing $file ..."
-        sh $file $email $setup_git $setup_folder_structure
+path=$(dirname $(realpath $0))
 
-        if [ $? -eq 1 ]; then
-            echo "Failed to install $file"
-            exit 1
-        fi
-    done
+execute_file () {
+    local file_name=$path/$1
+    echo "Installing $file ..."
+    sh $file_name $email $setup_git $setup_folder_structure
+
+    if [ $? -eq 1 ]; then
+        echo "Failed to install $file"
+        exit 1
+    fi
 }
 
-execute_file ./bin/pre-installers/*.sh
-execute_file ./bin/post-installers/*.sh
+execute_file pre-installers/setup.sh
+execute_file pre-installers/git.sh
+execute_file pre-installers/zsh.sh
+execute_file pre-installers/golang.sh
+execute_file pre-installers/python.sh
+execute_file pre-installers/rust.sh
+execute_file pre-installers/sdkman.sh
+execute_file post-installers/apps.sh
+execute_file post-installers/browsers.sh
+execute_file post-installers/docker.sh
+execute_file post-installers/font-manager.sh
+execute_file post-installers/java.sh
+execute_file post-installers/javascript.sh
+execute_file post-installers/latex.sh
+execute_file post-installers/lsp-servers.sh
+execute_file post-installers/lua.sh
+execute_file post-installers/neovim.sh
+execute_file post-installers/tmux-workspaces.sh
