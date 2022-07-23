92d61c9fcd6728ebbcd899044981b2d276cc146d
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-07-23 23:39:05 +0200

    add nala fetch to the beginning for optimal speed in download

--- bin/pre-installers/setup.sh
+++ bin/pre-installers/setup.sh
@@ -9,4 +9,6 @@ echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.l
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
 sudo apt install nala
 
+sudo nala fetch
+
 sudo nala install software-properties-common htop neofetch ncdu wget curl ripgrep tmux zip unzip feh fzf xkeycaps autokey-gtk nmap snapd gcc gnome-tweaks -y
