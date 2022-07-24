7a90d3f046e9da3b9138fee232a4ac92414f9903
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-07-24 15:46:54 +0200

    add stuff

---
+++ bin/post-installers/alacritty.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 -y
+
+rustup override set stable
+rustup update stable
+
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+
+cargo build --release
+
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+
+sudo cp target/release/alacritty /usr/local/bin
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+
+sudo mkdir -p /usr/local/share/man/man1
+gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+
+mkdir -p ${ZDOTDIR:-~}/.zsh_functions
+echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
+
+cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
+
+gsettings set org.gnome.desktop.default-applications.terminal exec 'alacritty'
+
+cd ..
+rm -rf alacritty
