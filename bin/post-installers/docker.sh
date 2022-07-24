eb921d47835f4504baa1969c7ea6da08e6766f3d
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-07-24 02:04:22 +0200

    use apt-get to install docker since nala doesnt have them

--- bin/post-installers/docker.sh
+++ bin/post-installers/docker.sh
@@ -1,18 +1,18 @@
 # https://docs.docker.com/engine/install/ubuntu/
 
 # Should be no way we have these but w/e
-sudo nala remove docker docker-engine docker.io containerd runc -y
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
 
-sudo nala update
-sudo nala install ca-certificates curl gnupg lsb-release -y
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo nala update
-sudo nala install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 # https://docs.docker.com/engine/install/linux-postinstall/
 # Allow none sudo docker
